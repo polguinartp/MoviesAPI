2c7c20d2cca2d7726c706e2950b7b45a8a8d5465
Pol Guinart <polgp120@hotmail.com>
2026-03-10 12:38:29 +0100

    Clean code.

--- MoviesAPI/Controllers/ShowtimeController.cs
+++ MoviesAPI/Controllers/ShowtimeController.cs
@@ -17,75 +17,75 @@ namespace MoviesAPI.Controllers;
 [Route("api/[controller]")]
 public class ShowtimeController : Controller
 {
-    private readonly IShowtimeService _service;
-    private readonly IMapper _mapper;
+	private readonly IShowtimeService _service;
+	private readonly IMapper _mapper;
 
-    public ShowtimeController(IShowtimeService service, IMapper mapper)
-    {
-        ArgumentNullException.ThrowIfNull(service);
-        ArgumentNullException.ThrowIfNull(mapper);
+	public ShowtimeController(IShowtimeService service, IMapper mapper)
+	{
+		ArgumentNullException.ThrowIfNull(service);
+		ArgumentNullException.ThrowIfNull(mapper);
 
-        _service = service;
-        _mapper = mapper;
-    }
+		_service = service;
+		_mapper = mapper;
+	}
 
-    [HttpGet("")]
-    [Authorize(Roles = Constants.Roles.Read)]
-    public async Task<ActionResult<IEnumerable<Showtime>>> GetAsync([FromQuery] DateTime date, [FromQuery] string movieTitle)
-    {
-        IEnumerable<ShowtimeEntity> entities = await _service.GetAsync(date, movieTitle);
-        var result = _mapper.Map<IEnumerable<Showtime>>(entities);
-        return Ok(result);
-    }
+	[HttpGet]
+	[Authorize(Roles = Constants.Roles.Read)]
+	public async Task<ActionResult<IEnumerable<Showtime>>> GetAsync([FromQuery] DateTime date, [FromQuery] string movieTitle)
+	{
+		var entities = await _service.GetAsync(date, movieTitle);
+		var result = _mapper.Map<IEnumerable<Showtime>>(entities);
+		return Ok(result);
+	}
 
-    [HttpGet("{id}")]
-    [Authorize(Roles = Constants.Roles.Read)]
-    public async Task<ActionResult<Showtime>> GetAsync(int id)
-    {
-        ShowtimeEntity showtime = await _service.GetByIdAsync(id);
-        return Ok(_mapper.Map<Showtime>(showtime));
-    }
+	[HttpGet("{id}")]
+	[Authorize(Roles = Constants.Roles.Read)]
+	public async Task<ActionResult<Showtime>> GetAsync(int id)
+	{
+		var showtime = await _service.GetByIdAsync(id);
+		return Ok(_mapper.Map<Showtime>(showtime));
+	}
 
-    [HttpPost("")]
-    [Authorize(Roles = Constants.Roles.Write)]
-    [ServiceFilter(typeof(ShowtimeActionFilter))]
-    public async Task<ActionResult<Showtime>> PostAsync([FromBody] Showtime showtime)
-    {
-        var result = await _service.CreateAsync(showtime);
-        return Ok(_mapper.Map<Showtime>(result));
-    }
+	[HttpPost]
+	[Authorize(Roles = Constants.Roles.Write)]
+	[ServiceFilter(typeof(ShowtimeActionFilter))]
+	public async Task<ActionResult<Showtime>> PostAsync([FromBody] Showtime showtime)
+	{
+		var result = await _service.CreateAsync(showtime);
+		return Ok(_mapper.Map<Showtime>(result));
+	}
 
-    [HttpPut("")]
-    [Authorize(Roles = Constants.Roles.Write)]
-    [ServiceFilter(typeof(ShowtimeActionFilter))]
-    public async Task<ActionResult<Showtime>> Put([FromBody] Showtime showtime)
-    {
-        if (await _service.GetByIdAsync(showtime.Id) == null)
-        {
-            return NotFound($"Not found Showtime with provided id");
-        }
+	[HttpPut]
+	[Authorize(Roles = Constants.Roles.Write)]
+	[ServiceFilter(typeof(ShowtimeActionFilter))]
+	public async Task<ActionResult<Showtime>> Put([FromBody] Showtime showtime)
+	{
+		if (await _service.GetByIdAsync(showtime.Id) is null)
+		{
+			return NotFound($"Not found Showtime with provided id");
+		}
 
-        var result = await _service.UpdateAsync(showtime);
-        return Ok(_mapper.Map<Showtime>(result));
-    }
+		var result = await _service.UpdateAsync(showtime);
+		return Ok(_mapper.Map<Showtime>(result));
+	}
 
-    [HttpDelete("{id}")]
-    [Authorize(Roles = Constants.Roles.Write)]
-    public async Task<ActionResult> Delete(int id)
-    {
-        if (await _service.GetByIdAsync(id) == null)
-        {
-            return NotFound($"Not found Showtime with provided id");
-        }
+	[HttpDelete("{id}")]
+	[Authorize(Roles = Constants.Roles.Write)]
+	public async Task<ActionResult> Delete(int id)
+	{
+		if (await _service.GetByIdAsync(id) is null)
+		{
+			return NotFound($"Not found Showtime with provided id");
+		}
 
-        await _service.DeleteAsync(id);
-        return Ok();
-    }
+		await _service.DeleteAsync(id);
+		return Ok();
+	}
 
-    [HttpPatch("")]
-    [Authorize(Roles = Constants.Roles.Write)]
-    public ActionResult Patch()
-    {
-        return StatusCode(StatusCodes.Status500InternalServerError, "Test error handler");
-    }
+	[HttpPatch]
+	[Authorize(Roles = Constants.Roles.Write)]
+	public ActionResult Patch()
+	{
+		return StatusCode(StatusCodes.Status500InternalServerError, "Test error handler");
+	}
 }
--- MoviesAPI/Services/ShowtimeService.cs
+++ MoviesAPI/Services/ShowtimeService.cs
@@ -15,107 +15,107 @@ namespace MoviesAPI.Services;
 
 public class ShowtimeService : IShowtimeService
 {
-    private readonly IRepository<ShowtimeEntity> _showtimeEntitiesRepository;
-    private readonly IRepository<MovieEntity> _movieEntitiesRepository;
-    private readonly IIMDBWebApiClient _webClient;
-    private readonly IMapper _mapper;
-    //private readonly IQueueService _queueService;
+	private readonly IRepository<ShowtimeEntity> _showtimeEntitiesRepository;
+	private readonly IRepository<MovieEntity> _movieEntitiesRepository;
+	private readonly IIMDBWebApiClient _webClient;
+	private readonly IMapper _mapper;
+	//private readonly IQueueService _queueService;
 
-    public ShowtimeService(IRepository<ShowtimeEntity> showtimeEntitiesRepository,
-        IRepository<MovieEntity> movieEntitiesRepository,
-        IIMDBWebApiClient webClient,
-        IMapper mapper,
-        IQueueService queueService)
-    {
-        ArgumentNullException.ThrowIfNull(showtimeEntitiesRepository);
-        ArgumentNullException.ThrowIfNull(webClient);
-        ArgumentNullException.ThrowIfNull(mapper);
-        ArgumentNullException.ThrowIfNull(movieEntitiesRepository);
-        ArgumentNullException.ThrowIfNull(queueService);
+	public ShowtimeService(IRepository<ShowtimeEntity> showtimeEntitiesRepository,
+			IRepository<MovieEntity> movieEntitiesRepository,
+			IIMDBWebApiClient webClient,
+			IMapper mapper,
+			IQueueService queueService)
+	{
+		ArgumentNullException.ThrowIfNull(showtimeEntitiesRepository);
+		ArgumentNullException.ThrowIfNull(webClient);
+		ArgumentNullException.ThrowIfNull(mapper);
+		ArgumentNullException.ThrowIfNull(movieEntitiesRepository);
+		ArgumentNullException.ThrowIfNull(queueService);
 
-        _showtimeEntitiesRepository = showtimeEntitiesRepository;
-        _webClient = webClient;
-        _mapper = mapper;
-        _movieEntitiesRepository = movieEntitiesRepository;
-        //_queueService = queueService;
-    }
+		_showtimeEntitiesRepository = showtimeEntitiesRepository;
+		_webClient = webClient;
+		_mapper = mapper;
+		_movieEntitiesRepository = movieEntitiesRepository;
+		//_queueService = queueService;
+	}
 
-    public async Task<IEnumerable<ShowtimeEntity>> GetAsync(DateTime date = default, string movieTitle = null)
-    {
-        IEnumerable<ShowtimeEntity> entities;
-        var includeProperties = "Movie";
+	public async Task<IEnumerable<ShowtimeEntity>> GetAsync(DateTime date = default, string movieTitle = null)
+	{
+		IEnumerable<ShowtimeEntity> entities;
+		var includeProperties = "Movie";
 
-        if (date == default && string.IsNullOrEmpty(movieTitle))
-        {
-            entities = await _showtimeEntitiesRepository.GetCollectionAsync(includeProperties: includeProperties);
-        }
-        else
-        {
-            var specification = Specification<ShowtimeEntity>.All;
-            if (date != default)
-            {
-                specification = specification.And(new ReleaseDateOlderThanSixMontsSpecification(date));
-            }
-            if (!string.IsNullOrEmpty(movieTitle))
-            {
-                specification = specification.And(new MovieTitleSpecification(movieTitle));
-            }
+		if (date == default && string.IsNullOrEmpty(movieTitle))
+		{
+			entities = await _showtimeEntitiesRepository.GetCollectionAsync(includeProperties: includeProperties);
+		}
+		else
+		{
+			var specification = Specification<ShowtimeEntity>.All;
+			if (date != default)
+			{
+				specification = specification.And(new ReleaseDateOlderThanSixMontsSpecification(date));
+			}
+			if (!string.IsNullOrEmpty(movieTitle))
+			{
+				specification = specification.And(new MovieTitleSpecification(movieTitle));
+			}
 
-            entities = await _showtimeEntitiesRepository.GetCollectionAsync(filter: specification.ToExpression(),
-                includeProperties: includeProperties);
-        }
+			entities = await _showtimeEntitiesRepository.GetCollectionAsync(filter: specification.ToExpression(),
+					includeProperties: includeProperties);
+		}
 
-        return entities;
-    }
+		return entities;
+	}
 
-    public async Task<ShowtimeEntity> GetByIdAsync(int id)
-    {
-        var includeProperties = "Movie";
-        return await _showtimeEntitiesRepository.GetByIdAsync(id, includeProperties);
-    }
+	public async Task<ShowtimeEntity> GetByIdAsync(int id)
+	{
+		var includeProperties = "Movie";
+		return await _showtimeEntitiesRepository.GetByIdAsync(id, includeProperties);
+	}
 
-    public async Task<ShowtimeEntity> CreateAsync(Showtime showtime)
-    {
-        var showtimeEntity = _mapper.Map<ShowtimeEntity>(showtime);
+	public async Task<ShowtimeEntity> CreateAsync(Showtime showtime)
+	{
+		var showtimeEntity = _mapper.Map<ShowtimeEntity>(showtime);
 
-        var movieInfo = await _webClient.GetMovieInfoAsync(showtime.Movie.ImdbId);
-        showtimeEntity.Movie = _mapper.Map<MovieEntity>(movieInfo);
+		var movieInfo = await _webClient.GetMovieInfoAsync(showtime.Movie.ImdbId);
+		showtimeEntity.Movie = _mapper.Map<MovieEntity>(movieInfo);
 
-        var movies = await _movieEntitiesRepository.GetCollectionAsync();
-        var existingMovie = movies.FirstOrDefault(x => x.Equals(showtimeEntity.Movie));
-        if (existingMovie != null)
-        {
-            showtimeEntity.Movie = existingMovie;
-        }
+		var movies = await _movieEntitiesRepository.GetCollectionAsync();
+		var existingMovie = movies.FirstOrDefault(x => x.Equals(showtimeEntity.Movie));
+		if (existingMovie != null)
+		{
+			showtimeEntity.Movie = existingMovie;
+		}
 
-        return await _showtimeEntitiesRepository.AddAsync(showtimeEntity);
-    }
+		return await _showtimeEntitiesRepository.AddAsync(showtimeEntity);
+	}
 
-    public async Task<ShowtimeEntity> UpdateAsync(Showtime showtime)
-    {
-        var existingEntity = await _showtimeEntitiesRepository.GetByIdAsync(showtime.Id);
-        _mapper.Map(showtime, existingEntity);
+	public async Task<ShowtimeEntity> UpdateAsync(Showtime showtime)
+	{
+		var existingEntity = await _showtimeEntitiesRepository.GetByIdAsync(showtime.Id);
+		_mapper.Map(showtime, existingEntity);
 
-        if (showtime.Movie != null && showtime.Movie.ImdbId != existingEntity.Movie.ImdbId)
-        {
-            var movieInfo = await _webClient.GetMovieInfoAsync(showtime.Movie.ImdbId);
-            existingEntity.Movie = _mapper.Map<MovieEntity>(movieInfo);
-        }
+		if (showtime.Movie != null && showtime.Movie.ImdbId != existingEntity.Movie.ImdbId)
+		{
+			var movieInfo = await _webClient.GetMovieInfoAsync(showtime.Movie.ImdbId);
+			existingEntity.Movie = _mapper.Map<MovieEntity>(movieInfo);
+		}
 
-        return await _showtimeEntitiesRepository.UpdateAsync(existingEntity);
-    }
+		return await _showtimeEntitiesRepository.UpdateAsync(existingEntity);
+	}
 
-    public async Task DeleteAsync(int id)
-    {
-        await _showtimeEntitiesRepository.DeleteAsync(id);
+	public async Task DeleteAsync(int id)
+	{
+		await _showtimeEntitiesRepository.DeleteAsync(id);
 
-        // aws free tier finished -> every 1M messages costs
-        //QueueMessage queueMessage = new QueueMessage()
-        //{
-        //    Id = Guid.NewGuid(),
-        //    Message = $"Showtime {id} has been deleted.",
-        //    DateTime = DateTime.Now
-        //};
-        //await _queueService.SendAsync(queueMessage);
-    }
+		// aws free tier finished -> every 1M messages costs
+		//QueueMessage queueMessage = new QueueMessage()
+		//{
+		//    Id = Guid.NewGuid(),
+		//    Message = $"Showtime {id} has been deleted.",
+		//    DateTime = DateTime.Now
+		//};
+		//await _queueService.SendAsync(queueMessage);
+	}
 }
